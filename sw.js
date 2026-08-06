fd1d2d0ff27b9ab1d61be5675aaccc0eb6f6a7b6
Łukasz Dudziński <lukaszdudzinski@github.com>
2026-08-06 14:01:49 +0200

    Bump version

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ukis-bodybuild-v.2026.8.6.06';
+const CACHE_NAME = 'ukis-bodybuild-v.2026.8.6.07';
 // Core assets that MUST be cached immediately
 const CORE_ASSETS = [
     './',
