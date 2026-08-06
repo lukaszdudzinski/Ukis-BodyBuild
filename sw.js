881c245035c5f4c6c204ed94a1500a47b4e19e41
Łukasz Dudziński <lukaszdudzinski@github.com>
2026-08-07 01:50:12 +0200

    fix: Bump Service Worker cache version to .04 to force update on clients

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ukis-bodybuild-v.2026.8.7.02';
+const CACHE_NAME = 'ukis-bodybuild-v.2026.8.7.04';
 // Core assets that MUST be cached immediately
 const CORE_ASSETS = [
     './',
