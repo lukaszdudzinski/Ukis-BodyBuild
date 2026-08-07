e07b65522d60138e6307a48c8e9c2bda73d25699
Łukasz Dudziński <lukaszdudzinski@github.com>
2026-08-07 13:22:24 +0200

    v.2026.8.7.19: Stabilna wersja i ratunek instalatora PWA

--- src/modules/ui/AppUI.js
+++ src/modules/ui/AppUI.js
@@ -1,4 +1,4 @@
-export const APP_VERSION = 'v.2026.8.7.18'; // <-- TEN NUMER ZMIENIAMY PRZY KAŻDEJ AKTUALIZACJI
+export const APP_VERSION = 'v.2026.8.7.19'; // <-- TEN NUMER ZMIENIAMY PRZY KAŻDEJ AKTUALIZACJI
 
 window.onerror = function(message, source, lineno, colno, error) {
     let errs = [];
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ukis-bodybuild-v.2026.8.7.18';
+const CACHE_NAME = 'ukis-bodybuild-v.2026.8.7.19';
 // Core assets that MUST be cached immediately
 const CORE_ASSETS = [
     './',
