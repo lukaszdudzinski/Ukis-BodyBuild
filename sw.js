5738225f5385bacc7c527cf6661e0f9886282d9a
Łukasz Dudziński <lukaszdudzinski@github.com>
2026-08-07 14:23:37 +0200

    Bump version to v.2026.8.7.21 to force PWA cache flush

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ukis-bodybuild-v.2026.8.7.20';
+const CACHE_NAME = 'ukis-bodybuild-v.2026.8.7.21';
 // Core assets that MUST be cached immediately
 const CORE_ASSETS = [
     './',
