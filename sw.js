01f8f1b33bf833670cceb6c465b7cf513b5d2a3e
Łukasz Dudziński <lukaszdudzinski@github.com>
2026-08-09 20:37:02 +0200

    Złamanie cache PWA: Aktualizacja nazwy Cache na 07

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ukis-bodybuild-v.2026.8.9.05';
+const CACHE_NAME = 'ukis-bodybuild-v.2026.8.9.07';
 // Core assets that MUST be cached immediately
 const CORE_ASSETS = [
     './',
