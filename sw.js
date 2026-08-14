68ce1783ce5f1154a6f4c1196f42dfe0185c6394
Łukasz Dudziński <lukaszdudzinski@github.com>
2026-08-14 08:28:26 +0200

    UX/UI Hotfix v2026.8.14.08: poprawa proporcji i paddingu powiększonych pól input w formularzu treningu

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ukis-bodybuild-v2026.8.14.07';
+const CACHE_NAME = 'ukis-bodybuild-v2026.8.14.08';
 // Core assets that MUST be cached immediately
 const CORE_ASSETS = [
     './',
